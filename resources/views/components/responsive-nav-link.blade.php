992a2b65d1ec4f95ee3ce41f635602f45cc8dc12
evgeniy <vyazovsky.ea@gmail.com>
2023-06-26 20:55:58 +0300

    format

--- resources/views/components/responsive-nav-link.blade.php
+++ resources/views/components/responsive-nav-link.blade.php
@@ -1,7 +1,11 @@
 @props(['active'])
 
 @php
-	$classes = $active ?? false ? 'block w-full pl-3 pr-4 py-2 border-l-4 border-indigo-400 text-left text-base font-medium text-indigo-700 bg-indigo-50 focus:outline-none focus:text-indigo-800 focus:bg-indigo-100 focus:border-indigo-700 transition duration-150 ease-in-out' : 'block w-full pl-3 pr-4 py-2 border-l-4 border-transparent text-left text-base font-medium text-gray-600 hover:text-gray-800 hover:bg-gray-50 hover:border-gray-300 focus:outline-none focus:text-gray-800 focus:bg-gray-50 focus:border-gray-300 transition duration-150 ease-in-out';
+	$shared = 'block w-full pl-3 pr-4 py-2 border-l-4 text-left text-base font-medium transition duration-150 ease-in-out';
+	$active = 'border-indigo-400 text-indigo-700 bg-indigo-50 focus:text-indigo-800 focus:bg-indigo-100 focus:border-indigo-700';
+	$inactive = 'border-transparent text-gray-600 hover:text-gray-800 hover:bg-gray-50 hover:border-gray-300 focus:text-gray-800 focus:bg-gray-50 focus:border-gray-300';
+	$stateClasses = $active ?? false ? $active : $inactive;
+	$classes = $shared . ' ' . $stateClasses;
 @endphp
 
 <a {{ $attributes->merge(['class' => $classes]) }}>
