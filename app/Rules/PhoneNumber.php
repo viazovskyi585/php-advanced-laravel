8b532a11f2dc19c94d6b1cc1df839506820529f1
evgeniy <vyazovsky.ea@gmail.com>
2023-06-29 17:46:26 +0300

    changed regex

--- app/Rules/PhoneNumber.php
+++ app/Rules/PhoneNumber.php
@@ -14,7 +14,7 @@ class PhoneNumber implements ValidationRule
      */
     public function validate(string $attribute, mixed $value, Closure $fail): void
     {
-        if (!preg_match('/^(\+\d{1,2}\s)?\(?\d{3}\)?[\s.-]?\d{3}[\s.-]?\d{4}$/', $value)) {
+        if (!preg_match('%^(?:(?:\(?(?:00|\+)([1-4]\d\d|[1-9]\d?)\)?)?[\-\.\ \\\/]?)?((?:\(?\d{1,}\)?[\-\.\ \\\/]?){0,})(?:[\-\.\ \\\/]?(?:#|ext\.?|extension|x)[\-\.\ \\\/]?(\d+))?$%i', $value) && strlen($value) >= 10) {
             $fail(__('Field must be a valid phone number.'));
         }
     }
