8eb45d73aac64f73ce4a8e87658a2422912358a5
evgeniy <vyazovsky.ea@gmail.com>
2023-07-10 17:36:22 +0300

    fixes

--- resources/views/admin/categories/categories-edit.blade.php
+++ resources/views/admin/categories/categories-edit.blade.php
@@ -1,4 +1,4 @@
-<x-admin-layout :breadcrumbs="[['text' => 'Categories', 'href' => route('admin.categories.index')], ['text' => 'Create']]">
+<x-admin-layout :breadcrumbs="[['text' => 'Categories', 'href' => route('admin.categories.index')], ['text' => 'Edit']]">
 	<div class="w-full max-w-full flex-none px-3">
 		<div
 			class="shadow-soft-xl relative mb-6 flex min-w-0 flex-col break-words rounded-2xl border-0 border-solid border-transparent bg-white bg-clip-border p-4">
