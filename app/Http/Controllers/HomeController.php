0407aea571036db90684377c3e14d38f15263436
evgeniy <vyazovsky.ea@gmail.com>
2023-07-24 10:22:08 +0300

    fix

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -13,7 +13,6 @@ class HomeController extends Controller
      */
     public function __invoke(Request $request)
     {
-        $categories = Category::take(6)->get();
         $products = Product::orderByDesc('id')->take(8)->get();
 
         return view('pages.home.home-page', compact('categories', 'products'));
